1382021b1b60065d63af52742f61a86ba92ef7b8
robihamdani <robi4us@gmail.com>
2019-07-21 08:13:18 +0700

    Save before ejecting

--- src/App.js
+++ src/App.js
@@ -1,62 +1,100 @@
 import React, { Component } from "react";
-
-import UserInput from "./UserInput/UserInput";
-import UserOutput from "./UserOutput/UserOutput";
+import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
   state = {
-    username: "max"
+    persons: [
+      { id: 1, name: "Max", age: "28" },
+      { id: 2, name: "Manu", age: "29" },
+      { id: 3, name: "Stephanie", age: "26" }
+    ],
+    showPersons: false
   };
 
-  userNameChangedHandler = event => {
-    this.setState({ username: event.target.value });
+  nameChangeHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = { ...this.state.persons[personIndex] };
+
+    // const person = Object.assign({}, this.state.persons[personIndex]);
+
+    person.name = event.target.value;
+
+    this.setState({
+      persons: [
+        { name: "Max", age: "28" },
+        { name: event.target.value, age: "29" },
+        { name: "Stephanie", age: "27" }
+      ]
+    });
+  };
+
+  deletePersonHandler = personIndex => {
+    // const persons = this.state.persons;
+
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
+  };
+
+  togglePersonHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
   };
 
   render() {
+    const style = {
+      backgroundColor: "green",
+      color: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer"
+    };
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                changed={event => this.nameChangeHandler(event, person.id)}
+              />
+            );
+          })}
+        </div>
+      );
+      style.backgroundColor = "red";
+    }
+
+    let classes = [""];
+
+    if (this.state.persons.length <= 2) {
+      classes.push("red");
+    }
+
+    if (this.state.persons.length <= 1) {
+      classes.push("bold");
+    }
+
     return (
       <div className="App">
-        <ol>
-          <li>Create TWO new components: UserInput and UserOutput</li>
-          <li>
-            UserInput should hold an input element, UserOutput two paragraphs
-          </li>
-          <li>
-            Output multiple UserOutput components in the App component (any
-            paragraph texts of your choice)
-          </li>
-          <li>
-            Pass a username (of your choice) to UserOutput via props and display
-            it there
-          </li>
-          <li>
-            Add state to the App component (=> the username) and pass the
-            username to the UserOutput component
-          </li>
-          <li>
-            Add a method to manipulate the state (=> an event-handler method)
-          </li>
-          <li>
-            Pass the event-handler method reference to the UserInput component
-            and bind it to the input-change event
-          </li>
-          <li>
-            Ensure that the new input entered by the user overwrites the old
-            username passed to UserOutput
-          </li>
-          <li>
-            Add two-way-binding to your input (in UserInput) to also display the
-            starting username
-          </li>
-          <li>
-            Add styling of your choice to your components/ elements in the
-            components - both with inline styles and stylesheets
-          </li>
-        </ol>
+        <h1>Hi, I'm react app</h1>
+        <p className={classes.join(" ")}>this is really working</p>
+        <button style={style} onClick={this.togglePersonHandler}>
+          Switch Name
+        </button>
 
-        <UserInput changed={this.userNameChangedHandler} />
-        <UserOutput userName={this.state.username} />
-        <UserOutput userName={this.state.username} />
-        <UserOutput userName="Max" />
+        {persons}
       </div>
     );
   }
---
+++ src/Person/Person.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Person.css";
+
+const Person = props => {
+  return (
+    <div className="Person">
+      <p onClick={props.click}>
+        I'm Person {props.name} and i'm {props.age} years old!
+      </p>
+      <p>{props.children}</p>
+
+      <input type="text" onChange={props.changed} value={props.name} />
+    </div>
+  );
+};
+
+export default Person;
